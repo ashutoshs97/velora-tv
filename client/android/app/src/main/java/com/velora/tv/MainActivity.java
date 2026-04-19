a40a95f8895f15562ff684ac98d582b7a7237cae
ashutoshs97 <87438951+ashutoshs97@users.noreply.github.com>
2026-04-19 16:54:23 +0530

    chore: scaffolded capacitor android app for fire tv and spatial navigation

---
+++ client/android/app/src/main/java/com/velora/tv/MainActivity.java
@@ -0,0 +1,5 @@
+package com.velora.tv;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,17 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { SettingsProvider } from './contexts/SettingsContext';
+import { initNavigation } from '@noriginmedia/norigin-spatial-navigation';
 import './index.css';
 import App from './App.jsx';
+
+// If running in Android Capacitor wrapper, enable TV Navigation
+if (window.navigator.userAgent.includes('Android') && window.innerWidth > 800) {
+  initNavigation({
+    debug: false,
+    visualDebug: false,
+  });
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
